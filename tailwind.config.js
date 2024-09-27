0313c430f7da61b76254d1210b71c8c82a240316
Iryna Chiripko <chiripko.irina@gmail.com>
2024-09-27 23:07:29 +0300

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,30 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {},
-  plugins: [],
+    darkMode: "class",
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            keyframes: {
+                wavey: {
+                    "0%, 100%": {
+                        transform: "scaleY(0.5)",
+                    },
+                    "50%": {
+                        transform: "scaleY(1.5)",
+                    },
+                },
+            },
+            animation: {
+                wavey: "wavey 1000ms linear infinite",
+            },
+        },
+    },
+    plugins: [
+        require("./plugins/openVariant"),
+        require("./plugins/animationDelay"),
+        require("./plugins/tableCaption"),
+        require("./plugins/buttonPlugin"),
+        require("./plugins/diagonalRadius"),
+        require("./plugins/outlineSolid"),
+    ],
 };
